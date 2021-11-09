4c63104ef864e6eaaacbea4e4a5a9c90817d5d77
ritesh.patel <patelr5@vrtx.com>
2021-11-08 22:49:04 -0500

    added comments

--- routes/checker/checkerRoutes.js
+++ routes/checker/checkerRoutes.js
@@ -3,6 +3,11 @@
  */
 const logger = require('../../logger/logger')
 
+/**
+ * Returns a sample message
+ * @param {*} req - request object
+ * @param {*} res - response object
+ */
 const sayHello = (req, res) => {
     logger.info(`Checker route! Saying hello from ${req.originalUrl}`)
     res.status(200).json({ "message": "Hello Express! I am ready for work!" })
