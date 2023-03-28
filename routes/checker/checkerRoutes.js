8ef835d00b9b8c782739426da73da07a9a0adfae
saspath <106750087+saspath@users.noreply.github.com>
2023-03-28 18:36:10 +0530

    Update checkerRoutes.js

--- routes/checker/checkerRoutes.js
+++ routes/checker/checkerRoutes.js
@@ -10,7 +10,7 @@ const logger = require('../../logger/logger')
  */
 const sayHello = (req, res) => {
     logger.info(`Checker route! Saying hello from ${req.originalUrl}`)
-    res.status(200).json({ "message": " Hello Express! I am ready for work!" })
+    res.status(200).json({ "message": "Hello Express! I am ready for work!" })
 }
 
 module.exports = {
